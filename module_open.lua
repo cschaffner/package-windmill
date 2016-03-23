0cd8871ced261dec4d99fa691e2e10105859aec6
Christian Schaffner <c.schaffner@uva.nl>
2016-03-23 22:01:04 +0100

    wip

--- module_open.lua
+++ module_open.lua
@@ -38,6 +38,8 @@ function M.run(duration, _, fn)
     local t = S
     local team_width = 400
     local score_width = 70
+    local x_games = 150
+    local x_standings = 1000
 
     -- HEADER
     a.add(anims.moving_font(t, E, 150, y, "Open Division", 80, 1,1,1,1))
@@ -50,11 +52,11 @@ function M.run(duration, _, fn)
         local game = open_data.games[idx]
 
 --        a.add(anims.moving_image(t, E, icons['gvb-icon'], 10, y, 140, y+60, 0.9))
-        a.add(anims.my_moving_font(t, E, 150, y, "flag:" .. game.team_1_country .. " " .. game.team_1 , 40, 1,1,1,1))
-        a.add(anims.my_moving_font(t, E, 150+team_width, y, "" .. game.team_1_score, 40, 1,1,1,1))
-        a.add(anims.my_moving_font(t, E, 150+team_width+score_width, y, "-", 40, 1,1,1,1))
-        a.add(anims.my_moving_font(t, E, 150+team_width+score_width+20, y, "" .. game.team_2_score , 40, 1,1,1,1))
-        a.add(anims.my_moving_font(t, E, 150+team_width+2*score_width+20, y, game.team_2 .. " flag:" .. game.team_1_country, 40, 1,1,1,1))
+        a.add(anims.my_moving_font(t, E, x_games, y, "flag:" .. game.team_1_country .. " " .. game.team_1 , 40, 1,1,1,1))
+        a.add(anims.my_moving_font(t, E, x_games+team_width, y, "" .. game.team_1_score, 40, 1,1,1,1))
+        a.add(anims.my_moving_font(t, E, x_games+team_width+score_width, y, "-", 40, 1,1,1,1))
+        a.add(anims.my_moving_font(t, E, x_games+team_width+score_width+20, y, "" .. game.team_2_score , 40, 1,1,1,1))
+        a.add(anims.my_moving_font(t, E, x_games+team_width+2*score_width+20, y, game.team_2 .. " flag:" .. game.team_1_country, 40, 1,1,1,1))
         y = y + 45
         t = t + 0.03
 
@@ -62,6 +64,27 @@ function M.run(duration, _, fn)
             break
         end
     end
+
+    for idx = 1, #open_data.standings do
+        local standing = open_data.standings[idx]
+        local
+
+--        a.add(anims.moving_image(t, E, icons['gvb-icon'], 10, y, 140, y+60, 0.9))
+        a.add(anims.my_moving_font(t, E, x_standings, y, standing.ranking , 40, 1,1,1,1))
+        a.add(anims.my_moving_font(t, E, x_standings, y, standing.team_name , 40, 1,1,1,1))
+        a.add(anims.my_moving_font(t, E, x_standings+team_width, y, "" .. standing.swiss_score, 40, 1,1,1,1))
+--        a.add(anims.my_moving_font(t, E, 150+team_width+score_width, y, "-", 40, 1,1,1,1))
+--        a.add(anims.my_moving_font(t, E, 150+team_width+score_width+20, y, "" .. game.team_2_score , 40, 1,1,1,1))
+--        a.add(anims.my_moving_font(t, E, 150+team_width+2*score_width+20, y, game.team_2 .. " flag:" .. game.team_1_country, 40, 1,1,1,1))
+        y = y + 45
+        t = t + 0.03
+
+--        if y > HEIGHT - 100 then
+--            break
+--        end
+    end
+
+
 
 --    a.add(anims.moving_image(S+1, E, icons['gvb-icon'], 1000, 400, 1000+300, 400+300, 1))
 
