17d6cf6fa8dba30d2c5efb6ca06e5b2fd292355a
Christian Schaffner <c.schaffner@uva.nl>
2016-03-23 22:59:40 +0100

    wip

--- anims.lua
+++ anims.lua
@@ -22,8 +22,8 @@ local function up_down_scroll(S, E, x, y, obj)
     local y = utils.make_smooth{
         {t = S,   val = y},
         {t = S+(E-S)/4,   val = y},
-        {t = S+(E-S)/2, val = y-1200, ease='step'},
-        {t = E-1,   val = y},
+        {t = S+(E-S)/2, val = y-900, ease='step'},
+        {t = E-(E-S)/4,   val = y, ease='step'},
         {t = E,   val = y},
     }
 
