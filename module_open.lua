d562866569a3610278608b04ee9556ab4a800954
Christian Schaffner <c.schaffner@uva.nl>
2016-03-23 22:56:00 +0100

    wip

--- module_open.lua
+++ module_open.lua
@@ -92,8 +92,8 @@ function M.run(duration, _, fn)
 --    a.add(anims.moving_image(S+1, E, icons['gvb-icon'], 1000, 400, 1000+300, 400+300, 1))
 
     fn.wait_t(0)
-    Scroller.hide(E-4)
-    Sidebar.hide(E-4)
+    Scroller.hide(E)
+    Sidebar.hide(E)
 
     for now in fn.upto_t(E) do
         a.draw(now)
