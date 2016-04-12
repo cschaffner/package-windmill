6669b11d74112a27a7c2af8c5c12685134075d89
Christian Schaffner <c.schaffner@uva.nl>
2016-04-13 00:14:59 +0200

    adjustments

--- module_open_playoff.lua
+++ module_open_playoff.lua
@@ -10,7 +10,7 @@ local gray = resource.create_colored_texture(0.28,0.28,0.28,1) -- gray
 --    return fname:sub(1,4) == "gvb-"
 --end)
 
-local background = resource.load_image("brackets.png")
+local background = resource.load_image("brackets_scaled.png")
 
 local open_data = {}
 
@@ -41,6 +41,7 @@ function M.run(duration, _, fn)
 
     local t = S
     local font_size = 40
+    local font_size_small = 30
     local team_width = 380
     local score_width = 70
     local x_games = 150
@@ -54,14 +55,15 @@ function M.run(duration, _, fn)
     local y_top = y
     t = t + 0.03
 
-    a.add(anims.moving_image(t, E, background, 00, y, 1816, y+1000, 1))
+    a.add(anims.moving_image(t, E, background, 0, y, 1920, y+909, 1))
+
 
     local pos = {}
 --    name     = team_1  ,  team_2 (relative to team_1),  start_time / field (relative to team_1)
-    pos["QF0"] = {100, 100, 0, 100, 100, 50}
-    pos["QF1"] = {100, 300, 0, 100, 100, 50}
-    pos["QF2"] = {1200, 100, 0, 100, 100, 50}
-    pos["QF3"] = {1200, 300, 0, 100, 100, 50}
+    pos["QF0"] = {39, 43, 0, 85, 40, 40}
+    pos["QF1"] = {39, 218, pos["QF0"][3], pos["QF0"][4], pos["QF0"][5], pos["QF0"][6]}
+    pos["QF2"] = {1614, pos["QF0"][2], pos["QF0"][3], pos["QF0"][4], pos["QF0"][5], pos["QF0"][6]}
+    pos["QF3"] = {1614, pos["QF1"][2], pos["QF0"][3], pos["QF0"][4], pos["QF0"][5], pos["QF0"][6]}
     pos["SF0"] = {600, 150, 0, 300, 100, 150}
     pos["SF1"] = {900, 150, 0, 300, 100, 150}
     pos["SF2"] = {600, 100, 0, 100, 100, 50}
@@ -72,14 +74,16 @@ function M.run(duration, _, fn)
     pos["Fin78"] = {800, 800, 200, 0, -100, 50}
 
 
+
     for idx = 1, #open_brackets do
         local game = open_brackets[idx]
         co = pos[game.name]
-        a.add(anims.my_moving_font(t, E, co[1], co[2], "flag:" .. game.team_1_country .. " " .. game.team_1 , font_size, 1,1,1,1))
-        a.add(anims.my_moving_font(t, E, co[1]+team_width, co[2], string.format("%2.0f", game.team_1_score), font_size, 1,1,1,1))
-        a.add(anims.my_moving_font(t, E, co[1]+co[3], co[2]+co[4], "flag:" .. game.team_2_country .. " " .. game.team_1 , font_size, 1,1,1,1))
-        a.add(anims.my_moving_font(t, E, co[1]+co[3]+team_width, co[2]+co[4], string.format("%2.0f", game.team_2_score), font_size, 1,1,1,1))
-        a.add(anims.my_moving_font(t, E, co[1]+co[5], co[2]+co[6], game.start_time .. " " .. game.field, font_size, 1,1,1,1))
+        a.add(anims.my_moving_font(t, E, co[1], y+co[2], "flag:" .. game.team_1_country .. " " .. game.team_1 , font_size, 1,1,1,1))
+        a.add(anims.my_moving_font(t, E, co[1]+team_width, y+co[2], string.format("%2.0f", game.team_1_score), font_size, 1,1,1,1))
+        a.add(anims.my_moving_font(t, E, co[1]+co[3], y+co[2]+co[4], "flag:" .. game.team_2_country .. " " .. game.team_1 , font_size, 1,1,1,1))
+        a.add(anims.my_moving_font(t, E, co[1]+co[3]+team_width, y+co[2]+co[4], string.format("%2.0f", game.team_2_score), font_size, 1,1,1,1))
+        a.add(anims.my_moving_font(t, E, co[1]+co[5], y+co[2]+co[6], game.start_time .. " " .. game.field, font_size_small, 1,1,1,1))
+        t = t + 0.03
     end
 
     fn.wait_t(0)
