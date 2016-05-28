5dbf4bd0728c03c3fee2cfed59905b40434d50db
Christian Schaffner <c.schaffner@uva.nl>
2016-05-28 20:34:23 +0200

    wip

--- module_shader.lua
+++ module_shader.lua
@@ -64,7 +64,7 @@ function M.run(duration, options, fn)
 --        transparent:use()
 --        gl.perspective(70, math.sin(sys.now()/2)*600, -500, math.cos(sys.now()/2)*600,
 --                           0, -100, 0)
-        gl.rotate(90, -1, 0, 0)
+--        gl.rotate(90, -1, 0, 0)
         gl.pushMatrix()
         blue:draw(200, 200, 400, 400, 1)
         gl.popMatrix()
