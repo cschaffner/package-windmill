0230edbafcf45c34b4afc1d19a542bf715566d58
Christian Schaffner <c.schaffner@uva.nl>
2016-05-29 21:37:36 +0200

    wip

--- module_weather.lua
+++ module_weather.lua
@@ -83,14 +83,19 @@ function M.run(duration, _, fn)
     a.add(anims.moving_image(t, E, radar_pics['weather_radar_background'], 200, y, 599+200, y+420, 1))
     a.add(function(t)
         if t > 1 and t < E-1 then
-            idx = math.ceil(t/E*#radar_data.times)
-    --        print(idx, radar_data[idx].filename)
-    --        gl.scale(10,10)
-    --        gl.translate(-200,-200)
-            return util.draw_correct(radar_pics[radar_data.times[idx].filename], 200, y, 200+600, y+420, 1)
---            return radar_pics[radar_data.times[idx].filename]:draw(200, y, 200+600, y+420, 1)
+            return util.draw_correct(radar_pics['weather_radar_00'], 200, y, 200+600, y+420, 1)
         end
     end)
+--    a.add(function(t)
+--        if t > 1 and t < E-1 then
+--            idx = math.ceil(t/E*#radar_data.times)
+--    --        print(idx, radar_data[idx].filename)
+--    --        gl.scale(10,10)
+--    --        gl.translate(-200,-200)
+--            return util.draw_correct(radar_pics[radar_data.times[idx].filename], 200, y, 200+600, y+420, 1)
+----            return radar_pics[radar_data.times[idx].filename]:draw(200, y, 200+600, y+420, 1)
+--        end
+--    end)
 
 --    local y_rain = HEIGHT-200
 --    local x_rain = 50
