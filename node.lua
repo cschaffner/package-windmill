a0c8e4f656df3280cbac07892315cdc964e28446
Christian Schaffner <c.schaffner@uva.nl>
2016-06-02 09:55:33 +0200

    new cb2

--- node.lua
+++ node.lua
@@ -332,8 +332,8 @@ Sidebar = (function()
 --
         local clock_x = utils.easeInOut(visibility, WIDTH-260, WIDTH-300)
         local clock_y = utils.easeInOut(visibility, HEIGHT-105, 850)
-        res.clockboarder2:draw(clock_x, clock_y-60, clock_x + 300, clock_y + 155)
-        res.font:write(clock_x + 150 - w/2, clock_y+5, time, 100, highlight_a(1))
+        res.clockboarder2:draw(clock_x, clock_y-30, clock_x + 250, clock_y + 125)
+        res.font:write(clock_x + 125 - w/2, clock_y+5, time, 100, highlight_a(1))
     end
 
     local current_speed = 0
