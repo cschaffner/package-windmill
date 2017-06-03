4772d93aff21d4272c0f1ee0e77f520007ae5306
Christian Schaffner <c.schaffner@uva.nl>
2017-06-03 17:29:22 +0200

    wip

--- node.lua
+++ node.lua
@@ -338,7 +338,7 @@ Sidebar = (function()
 --
         local clock_x = utils.easeInOut(visibility, WIDTH-220, WIDTH-272)
         local clock_y = utils.easeInOut(visibility, HEIGHT-205, 850)
-        res.clockboarder3:draw(clock_x, clock_y-10, clock_x + 230, clock_y + 105)
+        res.clockboarder3:draw(clock_x, clock_y-5, clock_x + 230, clock_y + 100)
         res.font:write(clock_x + 115 - w/2, clock_y+5, time, size, pink_a(1))
     end
 
