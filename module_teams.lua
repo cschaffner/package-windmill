aeb5383a5312e38dc08e344d936d1ee507fd6d2f
Christian Schaffner <c.schaffner@uva.nl>
2017-06-03 17:33:07 +0200

    wip

--- module_teams.lua
+++ module_teams.lua
@@ -45,13 +45,13 @@ function M.run(duration, args, fn)
     a.add(anims.my_moving_font(S,E, 200, y, "flag:".. args.team.country .. " " .. args.team.name, 80, 1,1,1,1))
     y = y + 80 + 20
     a.add(anims.my_moving_font(S,E, 200, y, "Division: ".. args.team.division, text_size, 1,1,1,1))
-    a.add(anims.my_moving_font(S,E, 500, y, "Current rank: ".. args.team.current_standing.ranking, text_size, 1,1,1,1))
+    a.add(anims.my_moving_font(S,E, 650, y, "Current rank: ".. args.team.current_standing.ranking, text_size, 1,1,1,1))
     y = y + text_size + 10
 
     if args.team.city ~= "" then
         a.add(anims.my_moving_font(S,E, 200, y, "City: ".. args.team.city, text_size, 1,1,1,1))
     end
-    a.add(anims.my_moving_font(S,E, 500, y, "Current Swiss Score: ".. args.team.current_standing.num_swiss_score, text_size, 1,1,1,1))
+    a.add(anims.my_moving_font(S,E, 650, y, "Current Swiss Score: ".. args.team.current_standing.num_swiss_score, text_size, 1,1,1,1))
     if args.team.current_standing.swiss_opponent_score_avg then
         a.add(anims.my_moving_font(S,E, 1100, y, "Avg Opp Swiss Score: ".. args.team.current_standing.swiss_opponent_score_avg, text_size, 1,1,1,1))
     end
